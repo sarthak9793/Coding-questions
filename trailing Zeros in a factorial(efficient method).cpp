530a08cbbee52f5d11b9de064e110a6e2ad83649
sarthak9793 <75237194+sarthak9793@users.noreply.github.com>
2022-10-07 23:39:33 +0530

    Create trailing Zeros in a factorial(efficient method).cpp

---
+++ trailing Zeros in a factorial(efficient method).cpp
@@ -0,0 +1,31 @@
+//                                                  trailing Zeros in a factorial(efficient method)
+/* A trailing zero is always produced by prime factors 2 and 5. If we can count the number of 5s and 2s, our task is done
+     n = 5: There is one 5 and 3 2s in prime factors of 5! (2 * 2 * 2 * 3 * 5). So a count of trailing 0s is 1.
+     n = 11: There are two 5s and eight 2s in prime factors of 11! (2 8 * 34 * 52 * 7). So the count of trailing 0s is 2.
+   We can easily observe that the number of 2s in prime factors is always more than or equal to the number of 5s. So if we count 5s in prime factors, we are done.
+   
+   How to count the total number of 5s in prime factors of n!? A simple way is to calculate floor(n/5). For example, 7! has one 5, 10! has two 5s. It is not done yet, 
+   there is one more thing to consider. Numbers like 25, 125, etc have more than one 5. For example, if we consider 28! we get one extra 5 and the 
+   number of 0s becomes 6. Handling this is simple, first, divide n by 5 and remove all single 5s, then divide by 25 to remove extra 5s, and so on. 
+*/
+// TBH I don't get it, but it's simple enough to remember
+#include<iostream>
+using namespace std;
+
+int trailingZeros(int num){
+    int count=0;
+    for(int i=5 ; num/i != 0 ; i=i*5){
+        count+=num/i;
+    }
+    return count;
+}
+
+int main(){
+    int num;
+    cout<<"Enter number: ";
+    cin>>num;
+    
+    cout<<"Number of trailing zeros in a factorial are: "<<trailingZeros(num);
+}
+// time complexity is 0(logn)
+
