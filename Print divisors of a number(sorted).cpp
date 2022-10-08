deb33aae6917c3e748e47b35e007c3fb84687edf
sarthak9793 <75237194+sarthak9793@users.noreply.github.com>
2022-10-08 22:33:54 +0530

    Update Print divisors of a number(sorted).cpp

--- Print divisors of a number(sorted).cpp
+++ Print divisors of a number(sorted).cpp
@@ -1,4 +1,12 @@
                                                             // Print divisors of a number(sorted)
+/* 30: (1,30), (2,15), (3,10), (5,6)
+   65: (1,65), (5,13)
+   25: (1,5), (5,5)
+   I notice some additional properties of these divisor pairs that will help hus to solve this problem
+        1) If the smaller pair elements are in ascending order the larger pair elements are automatically in descending order
+        2) The largest of the smaller pair element is less than or equal to than the smallest of the larger pair element
+*/
+   
 #include<iostream>
 #include<cmath>
 using namespace std;
@@ -27,4 +35,5 @@ int main(){
 }
 
 // Time complexity = O(2sqrt(n)) = O(sqrt(n))
+
 
