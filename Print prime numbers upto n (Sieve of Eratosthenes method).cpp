0c1725a67286965e9b4d1b283180e5cc6b830cf0
sarthak9793 <75237194+sarthak9793@users.noreply.github.com>
2022-10-08 22:32:09 +0530

    Update Print prime numbers upto n (Sieve of Eratosthenes method).cpp

--- Print prime numbers upto n (Sieve of Eratosthenes method).cpp
+++ Print prime numbers upto n (Sieve of Eratosthenes method).cpp
@@ -1,14 +1,12 @@
 //                                             Print prime numbers upto n (Sieve of Eratosthenes method)
-
-// We will take an array of size [num+1], which will represent all the numbers i.e. if they are prime or not. 
-// size = num+1 because suppose if num=3, then we want an array upto 3 i.e. 0,1,2,3 which is of size = 4
+// If we want to find prime numbers upto n, we will first create an array that runs from 0 to n i.e. its size should be n+1
+// The array will represent all the numbers i.e. if they are prime or not. 
 // marked(1) elements represent composite numbers and unmarked(0) elements represent prime numbers
 
 /* The idea of Sieve of Eratosthenes is that we will first take 2(because its the first prime number) and mark all multiples of 2, then we move on to the next 
    unmarked element which is 3 and mark all multiples of 3, then we move on to the next unmarked elements which is 5 and mark all multiples of 5 and so on*/
    
 #include<iostream>
-#include<cmath>
 using namespace std;
 
 void printPrime(int num){
@@ -38,5 +36,6 @@ int main(){
 
 // Time complexity = O(log(log(n))) : Its a big derivation so dont give a fuck about it
 //log(log(n)) grows very very slow, so we can say that its almost linear, this is much much better than the naive approach which had a time complexity = O(n^3/2)
+
 
 
